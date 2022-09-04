8b7c55a2bd1de4554a9a279619b6eec6fa91c03f
zhihong <zhihong.liu81@gmail.com>
2022-09-04 18:55:42 -0500

    add validation to edit review

--- app/forms/review_form.py
+++ app/forms/review_form.py
@@ -5,4 +5,4 @@ from app.models import Review
 
 class ReviewForm(FlaskForm):
     rating =IntegerField("rating", validators=[DataRequired(message='Rating is required')])
-    content = StringField("content",validators=[DataRequired(message='Zipcode is required'), Length( max=2000, message="Length of review content should be less than 2000 letters")])
+    content = StringField("content",validators=[DataRequired(message='Content is required'), Length( max=2000, message="Length of review content should be less than 2000 letters")])
