8787142e9e3115b1e987327fe39ea4934bd31138
zhihong <zhihong.liu81@gmail.com>
2022-10-04 20:54:22 -0500

    add edit and delete button on userprofile page

--- react-app/src/components/userProfile/userProfile.js
+++ react-app/src/components/userProfile/userProfile.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { NavLink, useHistory, useParams, Redirect } from 'react-router-dom';
+import { NavLink, useHistory, useLocation, Redirect } from 'react-router-dom';
 import { getProfileThunk } from '../../store/session';
 import './userProfile.css';
 import businessIcon from '../../images/businesses.jpg';
@@ -9,6 +9,9 @@ import ratingStarFilled from '../../images/rating-star-filled-1.png';
 import ratingStarEmpty from '../../images/rating-star-empty-1.png';
 import editIcon from '../../images/edit.png';
 import deleteIcon from '../../images/delete.png';
+import { Modal } from '../../context/Modal';
+import { deleteBusinessThunk } from '../../store/business';
+
 
 
 const GetUserProfile = () => {
@@ -17,8 +20,11 @@ const GetUserProfile = () => {
     const user = useSelector(state => state.session.user);
     const businesses = useSelector(state => state.business);
     const reviews = useSelector(state => state.review);
+    const location = useLocation();
+    const path = location.pathname;
 
     const [profileIsLoaded, setProfileIsLoaded] = useState(false);
+    const [showDeleteBusinessModal, setShowDeleteBusinessModal] = useState(-1);
 
     useEffect(() => {
         dispatch(getProfileThunk(user.id)).then(() => setProfileIsLoaded(true))
@@ -29,7 +35,7 @@ const GetUserProfile = () => {
 
     if (!user) {
         return <Redirect to='/' />;
-      }
+    }
 
 
     let businessesList = [];
@@ -55,10 +61,109 @@ const GetUserProfile = () => {
         return [averRating, totalReviews]
     }
 
-    const handleDeleteBusiness = () => {
-
+    const handleDeleteBusiness = (businessId, e) => {
+        e.preventDefault();
+        dispatch(deleteBusinessThunk(businessId)).then(() => history.push('/'))
     }
 
+    const timeFormated = (time) => {
+        const createdDate = new Date(time)
+        const date = createdDate.getDate();
+        const month = createdDate.getMonth() + 1;
+        const year = createdDate.getFullYear();
+
+        return `${month}/${date}/${year}`
+    }
+
+    const profileReviews = profileIsLoaded &&
+        (
+            <div className='profile-reviews'>
+                <h2>You have {reviewsList.length} {reviewsList.length === 1 ? 'review' : 'reviews'}</h2>
+                {reviewsList.map(review => (
+                    <div key={review.id} className='profile-review-container'>
+                        <div className='profile-review-business-container'>
+                            {review.business.images.length > 0 && <img id='profile-review-business-image' src={review.business.images[0].url} />}
+                            <div className='profile-business-info'>
+                                <h2 onClick={() => { history.push(`/businesses/${review.business.id}`) }}>{review.business.name}</h2>
+                                <div className='profile-business-address'>
+                                    <p>{review.business.address}</p>
+                                    <p>{review.business.city}, {review.business.state} {review.business.zipcode}</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='profile-review-review-info'>
+                            <div className='profile-review-rating'>
+                                {[1, 2, 3, 4, 5].map(ele => {
+                                    return (
+                                        <div key={ele} >
+                                            {review.rating >= ele ? <img alt='' src={ratingStarFilled} /> : <img alt='' src={ratingStarEmpty} />}
+                                        </div>
+                                    )
+                                })}
+                                <p>{timeFormated(review.createdAt)}</p>
+                            </div>
+                            <p>{review.content}</p>
+                            <div className='profile-review-edit-delete'>
+                                <img alt='' src={editIcon} />
+                                <img alt='' src={deleteIcon} />
+                            </div>
+
+                        </div>
+                    </div>
+                ))}
+            </div>
+        )
+
+
+    const profileBusinesses = profileIsLoaded &&
+        (
+            <div className='profile-businesses'>
+                <h2>You have {businessesList.length} {businessesList.length === 1 ? 'business' : 'businesses'}</h2>
+                {businessesList.map(business => (
+                    <div key={business.id} className='profile-business-container'>
+                        {business.images.length > 0 && <img className='' src={business.images[0].url} />}
+                        <div className='profile-business-info'>
+                            <h2 onClick={() => { history.push(`/businesses/${business.id}`) }}>{business.name}</h2>
+                            <div className='profile-business-rating-images'>
+                                {[1, 2, 3, 4, 5].map(ele => {
+                                    return (
+                                        <div key={ele} >
+                                            {businessRating(business)[0] >= ele ? <img alt='' src={ratingStarFilled} /> : <img alt='' src={ratingStarEmpty} />}
+                                        </div>
+                                    )
+                                })}
+                                <p>{businessRating(business)[0]}</p>
+                            </div>
+                            <p>{businessRating(business)[1]} {businessRating(business)[1] === 1 ? 'review' : 'reviews'}</p>
+                            <div className='profile-business-address'>
+                                <p>{business.address}</p>
+                                <p>{business.city}, {business.state} {business.zipcode}</p>
+                            </div>
+                            {user && user.id === business.userId &&
+                                <div className='profile-business-edit-delete'>
+                                    <NavLink to={`/businesses/${business.id}/edit`}><img alt='' src={editIcon} /></NavLink>
+                                    <img alt='' src={deleteIcon} onClick={() => setShowDeleteBusinessModal(business.id)} />
+                                </div>
+                            }
+                        </div>
+                        <>
+                            {showDeleteBusinessModal === business.id &&
+                                <Modal onClose={() => setShowDeleteBusinessModal(false)} >
+                                    <div className='delete-business-modal'>
+                                        <h2>Are you sure you want to delete: {business.name}?</h2>
+                                        <div >
+                                            <button className='delete-business-modal-delete-button' onClick={(e) => handleDeleteBusiness(business.id, e)}>Delete</button>
+                                            <button className='delete-business-modal-cancle-button' onClick={() => setShowDeleteBusinessModal(false)}>Cancle</button>
+                                        </div>
+                                    </div>
+
+                                </Modal>
+                            }
+                        </>
+                    </div>
+                ))}
+            </div>
+        )
 
 
     return (
@@ -68,67 +173,13 @@ const GetUserProfile = () => {
                 <h1>{user.firstName} {user.lastName[0].toUpperCase()}.</h1>
                 <div className='profile-user-business-review'>
                     <img className='profile-icon-image' alt='total reviews' src={reviewIcon} />
-                    <p>{reviewsList.length} {reviewsList.length === 1 ? 'review' : 'reviews'}</p>
+                    <p onClick={() => history.push('/user/reviews')}>{reviewsList.length} {reviewsList.length === 1 ? 'review' : 'reviews'}</p>
                     <img className='profile-icon-image' alt='total businesses' src={businessIcon} />
-                    <p>{businessesList.length} {businessesList.length === 1 ? 'business' : 'businesses'}</p>
+                    <p onClick={() => history.push('/user/businesses')}>{businessesList.length} {businessesList.length === 1 ? 'business' : 'businesses'}</p>
                 </div>
             </div>
             <div className='profile-review-business-container'>
-                <div className='profile-reviews'>
-                    <h2>You have {reviewsList.length} {reviewsList.length === 1 ? 'review' : 'reviews'}</h2>
-                    {reviewsList.map(review => (
-                        <div key={review.id}>
-                            <div className='profile-business-container'>
-                            {review.business.images.length > 0 && <img id='profile-review-business-image' src={review.business.images[0].url} />}
-                            <div className='profile-business-info'>
-                                <h2 onClick={() => { history.push(`/businesses/${review.business.id}`) }}>{review.business.name}</h2>
-                                <div className='profile-business-address'>
-                                    <p>{review.business.address}</p>
-                                    <p>{review.business.city}, {review.business.state} {review.business.zipcode}</p>
-                                </div>
-                            </div>
-                            </div>
-                            <div className='profile-review-review-info'>
-
-                            </div>
-
-
-                        </div>
-                    ))}
-
-                </div>
-                <div className='profile-businesses'>
-                    <h2>You have {businessesList.length} {businessesList.length === 1 ? 'business' : 'businesses'}</h2>
-                    {businessesList.map(business => (
-                        <div key={business.id} className='profile-business-container'>
-                            {business.images.length > 0 && <img className='' src={business.images[0].url} />}
-                            <div className='profile-business-info'>
-                                <h2 onClick={() => { history.push(`/businesses/${business.id}`) }}>{business.name}</h2>
-                                <div className='profile-business-rating-images'>
-                                    {[1, 2, 3, 4, 5].map(ele => {
-                                        return (
-                                            <div key={ele} >
-                                                {businessRating(business)[0] >= ele ? <img alt='' src={ratingStarFilled} /> : <img alt='' src={ratingStarEmpty} />}
-                                            </div>
-                                        )
-                                    })}
-                                    <p>{businessRating(business)[0]}</p>
-                                </div>
-                                <p>{businessRating(business)[1]} {businessRating(business)[1] === 1 ? 'review' : 'reviews'}</p>
-                                <div className='profile-business-address'>
-                                    <p>{business.address}</p>
-                                    <p>{business.city}, {business.state} {business.zipcode}</p>
-                                </div>
-                                {user && user.id === business.userId &&
-                                    <div className='profile-business-edit-delete'>
-                                        <NavLink to={`/businesses/${business.id}/edit`}><img alt='' src={editIcon}/></NavLink>
-                                        <img alt='' src={deleteIcon}/>
-                                    </div>
-                                }
-                            </div>
-                        </div>
-                    ))}
-                </div>
+                {path === '/user/reviews' ? profileReviews : profileBusinesses}
             </div>
         </div>
     )
