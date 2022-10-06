2260f05fcd2604dfd4d5deaa11d649aeb4966140
zhihong <zhihong.liu81@gmail.com>
2022-10-05 20:44:43 -0500

    add css to main page

--- react-app/src/components/getAllBusinesses/getAllBusinesses.js
+++ react-app/src/components/getAllBusinesses/getAllBusinesses.js
@@ -9,12 +9,21 @@ const GetAllBusinesses = () => {
     const dispatch = useDispatch();
     const businesses = useSelector(state => state.business);
     const [isLoaded, setIsLoaded] = useState(false);
-    const history = useHistory()
+    const history = useHistory();
+    const [imageId, setImageId] = useState(0);
 
     useEffect(() => {
         dispatch( getAllBusinessesThunk()).then(setIsLoaded(true));
 
     }, [dispatch])
+
+    useEffect(() => {
+        let counter = 0
+        const id = setInterval(() => {counter++; setImageId(counter % 4)}, 4000)
+
+        return () => clearInterval(id)
+
+    }, [])
 
 
     let businessList=[];
@@ -24,9 +33,17 @@ const GetAllBusinesses = () => {
         return null
     }
 
+    const images = {
+        0:"https://zhihong-capstone.s3.amazonaws.com/8e57fcc8cd554e77b2b4f123d78a73cf.jpg",
+        1:"https://zhihong-capstone.s3.amazonaws.com/b76e55cffab94b86a451e9891e80d5b1.jpg",
+        2:"https://zhihong-capstone.s3.amazonaws.com/2753747610e24ddd89bebe6052a2659d.jpg",
+        3:"https://zhihong-capstone.s3.amazonaws.com/7a922a742dc040ebaf26387e0c4f70e8.jpg"
+
+    }
+
     return ( isLoaded &&
         <div className='business-list-main-container'>
-            <img className='business-list-splash-image' alt='splash' src='https://res.cloudinary.com/zhihongliu/image/upload/v1661976915/steakhouse-2_oxmazi.jpg' />
+            <img className='business-list-splash-image' alt='splash' src={images[imageId]} />
             <h2>Your Next Review Awaits</h2>
             <div className='business-list-container'>
                 {businessList.map(business => (
